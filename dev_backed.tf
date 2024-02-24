75cd5ba3182bd3f8f00586139115af35cf36a4cc
Amaresh Pelleti <amareswer@hotmail.com>
2024-02-24 16:27:14 -0500

    updated backed file

---
+++ dev_backed.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "devopsdemobucket01"
+    key            = "state/terraform.tfstate"
+    region         = "ca-central-1"
+    encrypt        = true
+    dynamodb_table = "devtoolhub"
+  }
+
+}
